51c1b512aa485b49dcfd10b0533c82ebf57aaa65
Evan Li <evancli@gmail.com>
2014-03-28 00:26:18 -0700

    Updated license

--- BetrayalDie/src/com/evanli/betrayaldie/ArtificialDie.java
+++ BetrayalDie/src/com/evanli/betrayaldie/ArtificialDie.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of BetrayalDie.
+
+    BetrayalDie is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BetrayalDie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BetrayalDie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*  Created by Evan Li on 3/27/14.
+ *  Copyright (c) 2014 Evan Li. All rights reserved.
+ */
+
 package com.evanli.betrayaldie;
 
 import java.util.Random;
